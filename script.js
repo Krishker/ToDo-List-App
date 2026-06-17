2f58d261c062f625a335da4364b3faa4bd535202
Christopher Edeh <christopher.nnaemeka.edeh@gmail.com>
2026-06-17 15:39:31 +0200

    showTask

--- script.js
+++ script.js
@@ -4,20 +4,47 @@ const listContainer = document.getElementById("list-container");
 function addTask()
 {
     if (inputBox.value === '') {
-        alert("You must write something");        
+       alert("You must write something");        
     }
     else {
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7"
-        li.appendChild(span);
+       let li = document.createElement("li");
+       li.innerHTML = inputBox.value;
+       listContainer.appendChild(li);
+       let span = document.createElement("span");
+       span.innerHTML = "\u00d7"
+       li.appendChild(span);
     }
-
     inputBox.value = "";
+    saveData();
 
 }
+
+listContainer.addEventListener("click", function (e) {
+    
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData()
+        
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData()
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
+        /* 
+        function validateInput() {
+            if (inputBox.value.length > 2) {
+                inputBox.disabled = false;
+            }
+        } */
 
 
 
